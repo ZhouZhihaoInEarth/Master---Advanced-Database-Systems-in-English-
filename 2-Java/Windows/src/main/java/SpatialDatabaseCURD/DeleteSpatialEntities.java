030f56c4caddc8caf7498c4218e1049c59f71112
zhangmh59 <zhangmh59@outlook.com>
2023-12-01 00:35:00 +0100

    Almost delete

--- 2-Java/Windows/src/main/java/SpatialDatabaseCURD/DeleteSpatialEntities.java
+++ 2-Java/Windows/src/main/java/SpatialDatabaseCURD/DeleteSpatialEntities.java
@@ -1,36 +1,3 @@
-package SpatialDatabaseCURD;
-
-import ConfigSpatialDatabase.DatabaseConfigure;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public class DeleteSpatialEntities {
-    private final DatabaseConfigure connect_database;
-
-    public DeleteSpatialEntities(DatabaseConfigure connect_database) {
-        this.connect_database = connect_database;
-    }
-
-    public void DeleteEntity(int Entities_Number) {
-        String sql = "DELETE FROM Residential_Area WHERE Entities_Number = ?";
-
-        try (Connection connection = DriverManager.getConnection(connect_database.getUrl(), connect_database.getUsername(), connect_database.getPassword()); // 修正此处
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setInt(1, Entities_Number);
-
-            int affectedRows = preparedStatement.executeUpdate();
-
-            if (affectedRows > 0) {
-                System.out.println("Entity Deleted Successfully: Entity Number " + Entities_Number);
-            } else {
-                System.out.println("No Entity Found with Entity Number: " + Entities_Number);
-            }
-        } catch (SQLException e) {
-            System.out.println("Error deleting entity: " + e.getMessage());
-        }
-    }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4338ecb0ca99d81a43e57fde03f626ca9eee2691afc853daf3ba991c359a4b54
+size 1358
--- 2-Java/Windows/src/main/java/UserInterface/MainWindow.java
+++ 2-Java/Windows/src/main/java/UserInterface/MainWindow.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14ab0b5308afd01ecbb9402525c65d71c7b480a4b82e28bf548ac7e06217a260
-size 3316
+oid sha256:55acfec92fab70bc39526f24576808fed91e13d7beb061036da86e9be9ceb7b5
+size 4634
