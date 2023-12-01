3366322636e212ab0267bdf50fcca517fd174310
zhangmh59 <zhangmh59@outlook.com>
2023-12-01 12:40:21 +0100

    Finish Delete

--- 2-Java/Windows/src/main/java/SpatialDatabaseCURD/DeleteSpatialEntities.java
+++ 2-Java/Windows/src/main/java/SpatialDatabaseCURD/DeleteSpatialEntities.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4338ecb0ca99d81a43e57fde03f626ca9eee2691afc853daf3ba991c359a4b54
-size 1358
+oid sha256:83ece2748a1c6f7ad3698d2dab00620293004073510ae678f2a63d6ef512801b
+size 1294
--- 2-Java/Windows/src/main/java/UserInterface/MainWindow.java
+++ 2-Java/Windows/src/main/java/UserInterface/MainWindow.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55acfec92fab70bc39526f24576808fed91e13d7beb061036da86e9be9ceb7b5
-size 4634
+oid sha256:f463f43958ec014e8cb2b8c937d01483d00a0a73ffc35f74754f5a2b4639ebc3
+size 5161
