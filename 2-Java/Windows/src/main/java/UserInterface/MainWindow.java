0c6bc1cb1af7829e5f661b65c9ca575103b07925
ZhouZhihaoInEarth <monday_zhou@outlook.com>
2023-12-08 20:07:35 +0100

    finish equal

--- 2-Java/Windows/src/main/java/UserInterface/MainWindow.java
+++ 2-Java/Windows/src/main/java/UserInterface/MainWindow.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d63ffcec350fd3e64e98069b8806148642f6acda07fed904977809d67ec36472
-size 23433
+oid sha256:ffb0d4cbdd67a929dd67f8ff1419057bba02c3ba7290b0363cdbb6b66ee639f0
+size 28237
