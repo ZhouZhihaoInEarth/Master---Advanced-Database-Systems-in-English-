25bfe3e7e2608733c8c3b671a86d7174af454798
ZhouZhihaoInEarth <monday_zhou@outlook.com>
2023-12-09 00:21:04 +0100

    Optimization

--- 2-Java/Windows/src/main/java/UserInterface/MainWindow.java
+++ 2-Java/Windows/src/main/java/UserInterface/MainWindow.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa780717d41f379c810e43019788cf36dd2be6fece2c1989e4e395273ea38bf7
-size 34233
+oid sha256:aea9584abb5ea5d0c232f3b5da00ca8fb38cdd39eafaeb9d59a6bbaafb43459c
+size 38460
