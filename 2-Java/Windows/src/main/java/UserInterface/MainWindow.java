3d572a2f1c78d923eff3ce0a1edfdb8a95ef51a4
ZhouZhihaoInEarth <monday_zhou@outlook.com>
2023-12-09 14:42:18 +0100

    try to visualization

--- 2-Java/Windows/src/main/java/UserInterface/MainWindow.java
+++ 2-Java/Windows/src/main/java/UserInterface/MainWindow.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8fe8ce4d0d864d63e35e12eecd9e3a3aff82fe172dc7f6671877576a808e337
-size 47537
+oid sha256:6eb1fad7cc3b92fa639173fa216631d19bf5c723bbcfba6534502346cb05af81
+size 50450
